7140c542652c3c7fc2d9efbda9b918cfe250d260
yashnagla <naglayash@gmail.com>
2024-03-20 16:03:02 +0530

    Created

---
+++ function.js
@@ -0,0 +1,25 @@
+// Function Defination
+// function sum(a, b){ // a,b are parameters
+    // var total = a + b;
+    // console.log(total);
+// }
+// sum(10, 30); // Calling function // here 10,30 are arguments
+// sum(40, 30); // Calling function
+// sum(60, 30); // Calling function
+// sum(100, 30); // Calling function
+
+
+
+// function sum(a, b){
+//     return total = a + b;
+// }
+
+// var funExp = sum(10, 30);
+
+// console.log(funExp);
+
+
+var funExp = function(a, b){
+    return total = a + b;
+}
+console.log(funExp(10,15));
