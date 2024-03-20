540cabe1512b3cb1989c04f105459242e8413620
yashnagla <naglayash@gmail.com>
2024-03-20 15:33:07 +0530

    Added for loop

--- table.js
+++ table.js
@@ -1,6 +1,12 @@
-var i = 2;
-var num = 0;
-while(num <= 20){
+// var i = 2;
+// var num = 0;
+// while(num <= 20){
+//     console.log(i + " * " + num + " = " + i*num);
+//     num++;
+// }
+
+var i = 99;
+for(var num = 1; num <= 10; num++){
+    debugger;
     console.log(i + " * " + num + " = " + i*num);
-    num++;
 }
