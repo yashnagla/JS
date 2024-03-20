a6d335f3fb8720c482551a2f004e3e4092d37b8c
yashnagla <naglayash@gmail.com>
2024-03-20 15:30:51 +0530

    Added for loop

--- count.js
+++ count.js
@@ -1,11 +1,19 @@
+// While Loop
 // var num = 0;
 // while(num <= 100){
 //     console.log(num);
 //     num++;
 // }
 
-var num = 0;
-do{
+// Do While Loop
+// var num = 0;
+// do {
+//   console.log(num);
+//   num++;
+// } while (num <= 100);
+
+// For Loop
+for(var num = 0; num <= 100; num++){
+    debugger;
     console.log(num);
-    num++;
-}while(num <= 100)
+}
