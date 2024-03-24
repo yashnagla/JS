bb14aa07207822f83cc190c9d69d22444eeff08d
yashnagla <naglayash@gmail.com>
2024-03-24 13:36:28 +0530

    Created

---
+++ math.js
@@ -0,0 +1,52 @@
+// Math()
+// In JS mth object allows you to perform mathematical tasks on numbers.
+// console.log(Math.PI);  // returns the value of PI
+
+// let num = 10.2565;
+// console.log(Math.round(10.2565));
+
+// console.log(Math.pow(2,3)); // Here 2 is base and 3 is power
+
+// console.log(Math.sqrt(50));   // Returns square root of a number.
+// console.log(Math.sqrt(49));
+
+// Math.abs()
+// Math.abs(x) returns the absolute (+ve) value of x.
+
+// console.log(Math.abs(-55));
+// console.log(Math.abs(-55.5));
+// console.log(Math.abs(-955));
+// console.log(Math.abs(-4-6));
+
+// Math.ceil()
+// Math.ceil(x) returns the value of x rounded up to its nearest integer.
+// console.log(Math.ceil(99.1));
+// console.log(Math.round(99.1));
+
+// Math.floor()
+// Math.floor(x) returns the value of x rounded down to its nearest integer.
+
+// console.log(Math.floor(99.9));
+// console.log(Math.round(99.9));
+
+
+// Math.min()
+// Math.min(x) can be used to find the lowest value in a list of arguments
+// Math.max()
+
+// console.log(Math.min(1,150,30,-2,-200,8));
+
+// Math.max(x) can be used to find the lowest value in a list of arguments
+// console.log(Math.max(1,150,30,-2,-200,8));
+
+
+// Math.random()
+// Math.random(x) returns a random number between 0 (inclusive) and 1 (exclusive)
+// console.log(Math.floor(Math.random()*10));
+
+
+// Math.trunc()
+// Math.trunc(x) method returns the integer part of a number.
+
+console.log(Math.trunc(99.9999));
+console.log(Math.trunc(-4.6675434));
